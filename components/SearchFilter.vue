ad130305f250f631f567ae9d6c045a82d538ddef
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2021-01-22 21:29:29 +0100

    made filter a radio button list in desktop view made fitler section sticky, with auto scrolling input selection now cahnges with params

--- components/SearchFilter.vue
+++ components/SearchFilter.vue
@@ -16,7 +16,7 @@
         
             <Form @submit="apply">
                 
-                <select name="type">
+                <select v-if="$store.state.isMobile" name="type" v-on: >
                     <option value="">Alle Kategorien</option>
                     <option value="group">Gruppe/Verein</option>
                     <option value="therapist">Therapeut*in/Psychiater*in</option>
@@ -25,6 +25,19 @@
                     <option value="logopedics">Logopäd*in</option>
                     <option value="hairremoval">Haarentfernungsstudio</option>
                 </select>
+
+                <fieldset class="radio-buttons" v-else>
+                    <legend>Kategorien</legend>
+
+                    <Button light center radio="type" value="">Alle Kategorien</Button>
+                    <Button light center radio="type" value="group">Gruppe/Verein</Button>
+                    <Button light center radio="type" value="therapist">Therapeut*in/Psychiater*in</Button>
+                    <Button light center radio="type" value="endocrinologist">Endokrinologische Praxis</Button>
+                    <Button light center radio="type" value="surgeon">Operateur*in</Button>
+                    <Button light center radio="type" value="logopedics">Logopäd*in</Button>
+                    <Button light center radio="type" value="hairremoval">Haarentfernungsstudio</Button>
+                    
+                </fieldset>
                 
                 <Button center>
                     Anwenden
@@ -41,19 +54,121 @@
 import Form from "@/components/utils/Form";
 import Button from "@/components/utils/Button";
 import {ChevronRightIcon, MapPinIcon} from "vue-feather-icons";
+import EntryMixin from "@/mixins/entry";
 
 export default {
     name: "SearchFilter",
     components: {Button, Form, ChevronRightIcon, MapPinIcon},
+    mixins: [EntryMixin],
     data() {
         return {
-            expand: true
+            expand: true,
+            offset: 56,
+            listenerActive: false
         }
+    },
+    mounted() {
+        this.updateFormValues();
+
+        this.activateListener();
+    },
+    computed: {
+
+        // wrap state as computed, to make it watchable
+        isMobile() {
+            return this.$store.state.isMobile;
+        }
+
+    },
+    watch: {
+
+        isMobile (mobile) {
+            this.updateFormValues();
+
+            if (mobile) {
+                this.deactivateListener();
+            } else {
+                this.activateListener();
+            }
+        }
+
+    },
+    destroyed() {
+        this.deactivateListener();
     },
     methods: {
         
         apply: function(form) {
             this.$emit("apply", form);
+        },
+
+        async updateFormValues() {
+            let typeVal = this.$route.query.type;
+
+            await this.$nextTick();
+
+            let typeInput = this.$el.querySelector("select, fieldset");
+
+            if (!typeInput) return;
+
+            // XSS safeguard
+            if (this.typeMapping[typeVal ?? ""] === undefined) return;
+
+            if (typeInput.tagName === "SELECT") {
+                typeInput.value = typeVal ?? "";
+            } else {
+                let btn = typeInput.querySelector(`input${typeVal ? `[value=${typeVal}]` : '' }`);
+
+                if (btn) {
+                    btn.checked = true;
+                }
+
+            }
+
+        },
+
+        activateListener() {
+
+            if (!this.listenerActive) {
+                this.$addScrollEvent(this.scroll);              
+                this.listenerActive = true;
+                this.lastScroll = pageYOffset;
+                scroll();
+
+                console.log("Listener activated");
+            }
+
+        },
+
+        deactivateListener() {
+
+            this.$removeScrollEvent(this.scroll);
+            this.listenerActive = false;
+            this.$el.style.top = '';
+
+            console.log("Listener deactivated");
+
+        },
+
+        // if the browser window is too short, we might overflow
+        // to counter this, allow the user to scroll an overflowing SearchFilter
+        scroll(pos, dist) {
+
+            const offsetHeight = this.$el.offsetHeight;
+
+            const topOffset = 56;
+
+            let botOffset = window.innerHeight - offsetHeight;
+            if (botOffset > topOffset) botOffset = topOffset;
+
+            let newOffset = this.offset - dist;
+            if (newOffset > topOffset) newOffset = topOffset;
+            if (newOffset < botOffset) newOffset = botOffset;
+
+            this.offset = newOffset;
+
+            this.$el.style.top = newOffset + "px";
+
         }
         
     }
@@ -63,13 +178,12 @@ export default {
 <style scoped>
 
 .search-filter {
-    margin: 0 0 20px 0;
+    padding: 16px 0;
     display: flex;
     flex-direction: column;
-    position: sticky;
     height: auto;
     align-self: start;
-    top: 72px;
+    position: sticky;
 }
 
 .search-filter > .bar {
@@ -97,6 +211,23 @@ export default {
     100% {
         height: 0;
     }
+}
+
+fieldset {
+    margin: 4px;
+    border: 0;
+}
+
+.radio-buttons >>> .button {
+    border-radius: 0px;
+}
+
+.radio-buttons label:first-of-type >>> .button {
+    border-radius: 8px 8px 0 0;
+}
+
+.radio-buttons label:last-of-type >>> .button {
+    border-radius: 0 0 8px 8px;
 }
 
 .search-filter.expand > .filter {
