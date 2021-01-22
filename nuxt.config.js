c988e6be5c1806dfbde2f0b92989bff7e0fab10a
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2021-01-22 21:14:53 +0100

    add debug sourcemaps

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,11 @@ export default {
 
     // Build Configuration (https://go.nuxtjs.dev/config-build)
     build: {
+        extend(config, ctx) {
+            if (ctx.isDev) {
+                config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+            }
+        }
     },
 
     // Env config for plugins
