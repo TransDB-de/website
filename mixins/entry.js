de14ac3328fce20cc74ccb49d01619f8da323933
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2021-01-23 22:48:35 +0100

    restructured entry mixin mappings now sorted by types

--- mixins/entry.js
+++ mixins/entry.js
@@ -11,33 +11,44 @@ export default {
                 logopedics: "Logopäd*in",
                 hairremoval: "Haarentfernungsstudio"
             },
-            offerMapping: {
-                indication: "Indikationen",
-                therapy: "Begleittherapie",
-                expertise: "Gutachten",
-                mastectomy: "Mastektomie",
-                vaginPI: "Vaginoplastie (penile Inversion)",
-                vaginCombined: "Vaginoplastie (kombinierte Methode)",
-                ffs: "Gesichtsfeminisierende OP's (FFS)",
-                penoid: "Penoidaufbau",
-                breast: "Brustaufbau",
-                hyst: "Hysterektomie",
-                orch: "Orchiektomie",
-                clitPI: "Klitorispenoid / Metoidioplastik",
-                bodyfem: "Körperfemininisierende OP's"
+            
+            attributeMapping: {
+                group: {
+                    trans: "Transfokus",
+                    regularMeetings: "Regelmäßige Treffen",
+                    consulting: "Beratungsangebot",
+                    activities: "Freizeitangebote"
+                },
+                hairremoval: {
+                    insurancePay: "Kostenübernahme",
+                    transfriendly: "Transfreundlich",
+                    hasDoctor: "Ärztlich"
+                }
             },
+            
+            offerMapping: {
+                therapist: {
+                    indication: "Indikationen",
+                    therapy: "Begleittherapie",
+                    expertise: "Gutachten"
+                },
+                surgeon: {
+                    mastectomy: "Mastektomie",
+                    vaginPI: "Vaginoplastie (penile Inversion)",
+                    vaginCombined: "Vaginoplastie (kombinierte Methode)",
+                    ffs: "Gesichtsfeminisierende OP's (FFS)",
+                    penoid: "Penoidaufbau",
+                    breast: "Brustaufbau",
+                    hyst: "Hysterektomie",
+                    orch: "Orchiektomie",
+                    clitPI: "Klitorispenoid / Metoidioplastik",
+                    bodyfem: "Körperfemininisierende OP's"
+                }
+            },
+            
             subjectMapping: {
                 therapist: "Psychologische*r Psychotherapeut*in",
                 psychologist: "Psychiater (Facharzt für Psychiatrie)"
-            },
-            attributeMapping: {
-                insurancePay: "Kostenübernahme",
-                transfrendly: "Transfreundlich",
-                hasDoctor: "Ärztlich",
-                trans: "Transfokus",
-                regularMeetings: "Regelmäßige Treffen",
-                consulting: "Beratungsangebot",
-                activities: "Freizeitangebote"
             }
         }
     }
