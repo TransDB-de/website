65d46c93892dd91aae9255391d9401ff29900d9e
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2021-01-27 19:02:45 +0100

    small performance improvement mixin constants now constant values, instead of reactive variables

--- mixins/entry.js
+++ mixins/entry.js
@@ -1,56 +1,59 @@
+const typeMapping = {
+    "": "Alle Kategorien",
+    group: "Gruppe/Verein",
+    therapist: "Therapeut*in/Psychiater*in",
+    endocrinologist: "Endokrinologische Praxis",
+    surgeon: "Operateur*in",
+    logopedics: "Logopäd*in",
+    hairremoval: "Haarentfernungsstudio"
+}
+
+const attributeMapping = {
+    group: {
+        trans: "Transfokus",
+        regularMeetings: "Regelmäßige Treffen",
+        consulting: "Beratungsangebot",
+        activities: "Freizeitangebote"
+    },
+    hairremoval: {
+        insurancePay: "Kostenübernahme",
+        transfriendly: "Transfreundlich",
+        hasDoctor: "Ärztlich"
+    }
+}
+
+const offerMapping = {
+    therapist: {
+        indication: "Indikationen",
+        therapy: "Begleittherapie",
+        expertise: "Gutachten"
+    },
+    surgeon: {
+        mastectomy: "Mastektomie",
+        vaginPI: "Vaginoplastie (penile Inversion)",
+        vaginCombined: "Vaginoplastie (kombinierte Methode)",
+        ffs: "Gesichtsfeminisierende OP's (FFS)",
+        penoid: "Penoidaufbau",
+        breast: "Brustaufbau",
+        hyst: "Hysterektomie",
+        orch: "Orchiektomie",
+        clitPI: "Klitorispenoid / Metoidioplastik",
+        bodyfem: "Körperfemininisierende OP's"
+    }
+}
+
+const subjectMapping = {
+    therapist: "Psychologische*r Psychotherapeut*in",
+    psychologist: "Psychiater (Facharzt für Psychiatrie)"
+}
+
 export default {
 
-    data() {
-        return {
-            typeMapping: {
-                "": "Alle Kategorien",
-                group: "Gruppe/Verein",
-                therapist: "Therapeut*in/Psychiater*in",
-                endocrinologist: "Endokrinologische Praxis",
-                surgeon: "Operateur*in",
-                logopedics: "Logopäd*in",
-                hairremoval: "Haarentfernungsstudio"
-            },
-            
-            attributeMapping: {
-                group: {
-                    trans: "Transfokus",
-                    regularMeetings: "Regelmäßige Treffen",
-                    consulting: "Beratungsangebot",
-                    activities: "Freizeitangebote"
-                },
-                hairremoval: {
-                    insurancePay: "Kostenübernahme",
-                    transfriendly: "Transfreundlich",
-                    hasDoctor: "Ärztlich"
-                }
-            },
-            
-            offerMapping: {
-                therapist: {
-                    indication: "Indikationen",
-                    therapy: "Begleittherapie",
-                    expertise: "Gutachten"
-                },
-                surgeon: {
-                    mastectomy: "Mastektomie",
-                    vaginPI: "Vaginoplastie (penile Inversion)",
-                    vaginCombined: "Vaginoplastie (kombinierte Methode)",
-                    ffs: "Gesichtsfeminisierende OP's (FFS)",
-                    penoid: "Penoidaufbau",
-                    breast: "Brustaufbau",
-                    hyst: "Hysterektomie",
-                    orch: "Orchiektomie",
-                    clitPI: "Klitorispenoid / Metoidioplastik",
-                    bodyfem: "Körperfemininisierende OP's"
-                }
-            },
-            
-            subjectMapping: {
-                therapist: "Psychologische*r Psychotherapeut*in",
-                psychologist: "Psychiater (Facharzt für Psychiatrie)"
-            }
-        }
+    created() {
+        this.typeMapping = typeMapping;
+        this.attributeMapping = attributeMapping;
+        this.offerMapping = offerMapping;
+        this.subjectMapping = subjectMapping;
     }
 
 }
