52c5ef3612044261497daf677ec4c397a6b3d4a5
Lena Emme <feuerhamster02@gmail.com>
2022-01-10 20:19:47 +0100

    Changing error message for expired login

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,11 +2,14 @@ import config from "$lib/config"
 import axios from "axios"
 
 import { goto } from "$app/navigation"
+import { popupWarn } from "$components/popup.svelte"
 
 axios.interceptors.response.use((res) => res, (err) => {
 	// Axios interceptor (middleware) to redirect unauthorized requests to login page
 	if (err.response.status === 401 && !window.location.pathname.includes("/login")) {
 		goto("/login");	
+		popupWarn("Bitte melde dich erneut an");
+		return;
 	}
 	
 	if (err.response.status === 403 && err.response.data.error === "invalid_csrf_token") {
