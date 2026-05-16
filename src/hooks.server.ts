7f3696797d12ebe53f5e158047e44d88f028184a
Lena Emme <38376566+Feuerhamster@users.noreply.github.com>
2026-05-17 00:12:32 +0200

    fix language header error bug

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,16 @@ import type { Handle } from "@sveltejs/kit";
 import { env } from "$env/dynamic/private";
 import jwt from "jsonwebtoken";
 import langHeaderParser from "accept-language-parser";
+import { locales } from "$lib/localization.svelte";
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const acceptLang = event.request.headers.get("Accept-Language");
 	const langs = acceptLang ? langHeaderParser.parse(acceptLang) : [];
 	let preferredLang = langs[0]?.code ?? "de";
+
+	if (!locales.includes(preferredLang)) {
+		preferredLang = locales[1];
+	}
 
 	const cookieLang = event.cookies.get("preferred-lang");
 	if (cookieLang) {
